3476ef65ac9e64fcf7b2373dff589482b9548294
Igor_Gruzdo <igruzdo@yandex.ru>
2022-03-19 14:09:29 +0300

    fix: fixed zero before date and time if it less than 10

--- 02_hello/date.js
+++ 02_hello/date.js
@@ -1,8 +1,15 @@
 const currentDateTime =  () => {
     const newDate = new Date()
+    const year = `${newDate.getFullYear()}`
+    const month = newDate.getMonth() >= 10 ? `${newDate.getMonth()}` : `0${newDate.getMonth()}`
+    const day = newDate.getDate() >= 10 ? `${newDate.getDate()}` : `0${newDate.getDate()}`
+    const hour = newDate.getHours() >= 10 ? `${newDate.getHours()}` : `0${newDate.getHours()}`
+    const minutes = newDate.getMinutes() >= 10 ? `${newDate.getMinutes()}` : `0${newDate.getMinutes()}`
+    const seconds = newDate.getSeconds() >= 10 ? `${newDate.getSeconds()}` : `0${newDate.getSeconds()}`
+
     const date = {
-        date: `${newDate.getFullYear()}-${newDate.getMonth()}-${newDate.getDate()}`,
-        time: `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`
+        date: `${year}-${month}-${day}`,
+        time: `${hour}:${minutes}:${seconds}`
     }
 
     return date
